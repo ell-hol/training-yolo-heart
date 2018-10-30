fae21f5d728ace3e161fd8994dbb32a69d7304e2
unknown <BU48896809@univ-lyon1.fr>
2018-10-30 17:30:22 +0100

    Added some results

--- decompress_data.py
+++ decompress_data.py
@@ -3,3 +3,4 @@ import tarfile
 
 tar = tarfile.open(os.path.join(os.getcwd(), 'yzahidi-datasets-heart-challenge.tar'))
 tar.extractall(path=os.getcwd())
+print("data has been decompressed in directory {}/data".format(os.getcwd()))
